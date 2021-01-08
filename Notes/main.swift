c74e677bb1b2462e2d486e295796c20b69babe53
Devon Hanses <dhansen@lcs.on.ca>
2021-01-08 12:12:08 -0500

    Initial Commit

---
+++ Notes/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Notes
+//
+//  Created by Devon Hansen on 2021-01-08.
+//  Copyright © 2021 Devon Hansen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
