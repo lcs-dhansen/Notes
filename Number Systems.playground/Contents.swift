402a0d2fb5968ec33032a3bd8a21ed99d69b3a39
Devon Hanses <dhansen@lcs.on.ca>
2021-01-08 12:26:19 -0500

    Found the binary value of 27 using % and / decimal - binary

---
+++ Number Systems.playground/Contents.swift
@@ -0,0 +1,37 @@
+import Cocoa
+
+var str = "Hello, playground"
+
+//find the digits that represent 27 in binary this is; 11011 in base 2
+
+//use the remainder operator to get the first binary digit
+//The remainder operator is the % sign
+27 % 2
+
+//find the decimla value left to convert
+//findthe division operator is/
+27 / 2
+
+//get next binary digit
+13 % 2
+//Get vaue left to convert
+13 / 2
+
+//get next binary digit
+6 % 2
+
+//value left
+6 / 2
+
+//next digit
+3 % 2
+
+//value left
+3 / 2
+
+//next
+1 % 2
+
+//value
+1 / 2
+//done now because demimal remainder left is 0 0 cant be divided by 2 into a new number.
